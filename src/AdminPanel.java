7aff011bd3d85bf3b08876dfe7355e6c604a485f
Nora-1212 <yshi25@student.oulu.fi>
2026-04-03 19:54:08 +0800

    test

--- src/AdminPanel.java
+++ src/AdminPanel.java
@@ -1,8 +1,8 @@
 import java.util.*;
 
 public class AdminPanel {
-    private LinkedList<RegisteredUsers> registeredUsersList = new LinkedList<>();
-    private UserService userService = new UserService(registeredUsersList);
+    LinkedList<RegisteredUsers> registeredUsersList = new LinkedList<>();
+    UserService userService = new UserService(registeredUsersList);
     Scanner scanner = new Scanner(System.in);
     public void userManagementOptions(){
         while (true){
@@ -11,10 +11,13 @@ public class AdminPanel {
                     "2.View Registered Users\n" +
                     "3.Remove Registered Users\n" +
                     "4.Update Registered Users\n" +
-                    "5.EXIT");
+                    "5.EXIT\n" +
+                    "6. View System Logs\n" +
+                    "7. Manage Pending Bike Requests");
 
             int choice;
             choice = Integer.parseInt(scanner.nextLine());
+            BikeService bikeService = new BikeService();
 
             switch (choice){
                 case 1:
@@ -33,11 +36,48 @@ public class AdminPanel {
                     System.out.println("Exiting program...");
                     scanner.close();
                     return;
+                case 6:
+                    bikeService.viewLogs();
+                    break;
+                case 7:
+                    manageRequests();
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
             }
 
         }
 
 
+    }
+    public void manageRequests(){
+        BikeService bikeService = new BikeService();
+        Scanner sc = new Scanner(System.in);
+        while (true){
+            System.out.println("1. View Queue\n" +
+                    "2. Update Queue (Remove First)\n" +
+                    "3. Exit");
+            if (!sc.hasNextInt()){
+                System.out.println("Invalid input. Please enter a number.");
+                sc.next();
+                continue;
+            }
+
+            int c = sc.nextInt();
+            sc.nextLine();
+
+            if (c == 1){
+                bikeService.Queue();
+            } else if (c == 2) {
+                bikeService.removeFirstRequest();
+            } else if (c == 3) {
+                break;
+            } else {
+                System.out.println("Invalid choice. Please try again.");
+            }
+
+            sc.close();
+        }
     }
 
 
--- src/BikeService.java
+++ src/BikeService.java
@@ -1,9 +1,46 @@
 import java.time.LocalDateTime;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.*;
 
 public class BikeService {
+    private Stack<ERyderLog> logStack = new Stack<>();
+
+    public void addLog(String logId, String event) {
+        ERyderLog eryderLog=new ERyderLog(logId,event,LocalDateTime.now());
+        logStack.push(eryderLog);
+    }
+
+    public void viewLogs() {
+        if (!logStack.isEmpty()){
+            for (ERyderLog eryderLog : logStack){
+                System.out.println(eryderLog);
+            }
+        } else {
+            System.out.println("No system logs available.");
+        }
+    }
+
+    private Queue<ActiveRental> bikeRequestQueue = new ArrayDeque<>();
+
+    public void addToQueue(String bikeID, String email,String location) {
+        ActiveRental req = new ActiveRental(bikeID,email,LocalDateTime.now());
+        bikeRequestQueue.add(req);
+    }
+
+    private String emailAddress;
+    private LocalDateTime tripStartTime;
+    private boolean locationValid;
+    UserRegistration userRegistration = new UserRegistration();
+    LinkedList<ActiveRental> activeRentalsList = new LinkedList<>();
+
+    public void viewActiveRentals() {
+        if (activeRentalsList.isEmpty()){
+            System.out.println("No active rentals at the moment.");
+        } else {
+            for (ActiveRental activeRental : activeRentalsList){
+                System.out.println(activeRental);
+            }
+        }
+    }
     public void releaseBike(String bikeID) {
         for (Bike bike : BikeDatabase.bikes) {
             if (bike.getBikeID().equals(bikeID)) {
@@ -38,6 +75,19 @@ public class BikeService {
                 System.out.println("Reserving the bike with the " + bikeID + ".\nPlease follow the on-screen direction to locate the bike and start your trip.\n");
                 break;
             }
+        }
+    }
+
+    public void Queue(){
+        System.out.println("=== Pending Requests ===");
+        for (ActiveRental req : bikeRequestQueue){
+            System.out.println(req);
+        }
+    }
+
+    public void removeFirstRequest(){
+        if (!bikeRequestQueue.isEmpty()){
+            bikeRequestQueue.remove();
         }
     }
 }
